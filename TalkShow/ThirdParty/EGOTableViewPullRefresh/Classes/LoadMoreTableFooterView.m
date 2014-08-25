89b0239e147a0816ff31fba21ed33cbc7c77bf04
shisanlang <shisanlangd@gmail.com>
2014-08-25 15:56:44 +0800

    re

--- TalkShow/ThirdParty/EGOTableViewPullRefresh/Classes/LoadMoreTableFooterView.m
+++ TalkShow/ThirdParty/EGOTableViewPullRefresh/Classes/LoadMoreTableFooterView.m
@@ -113,7 +113,8 @@
 	switch (aState) {
 		case EGOOPullPulling:
 			
-			_statusLabel.text = NSLocalizedStringFromTable(@"Release to load more...",@"PullTableViewLan", @"Release to load more status");
+//			_statusLabel.text = NSLocalizedStringFromTable(@"Release to load more...",@"PullTableViewLan", @"Release to load more status");
+            _statusLabel.text = NSLocalizedStringFromTable(@"释放加载更多...",@"PullTableViewLan", @"Release to load more status");
 			[CATransaction begin];
 			[CATransaction setAnimationDuration:FLIP_ANIMATION_DURATION];
 			_arrowImage.transform = CATransform3DIdentity;
@@ -129,7 +130,8 @@
 				[CATransaction commit];
 			}
 			
-			_statusLabel.text = NSLocalizedStringFromTable(@"Pull up to load more...", @"PullTableViewLan",@"Pull down to load more status");
+//			_statusLabel.text = NSLocalizedStringFromTable(@"Pull up to load more...", @"PullTableViewLan",@"Pull down to load more status");
+            _statusLabel.text = NSLocalizedStringFromTable(@"上拉加载更多...", @"PullTableViewLan",@"Pull down to load more status");
 			[_activityView stopAnimating];
 			[CATransaction begin];
 			[CATransaction setValue:(id)kCFBooleanTrue forKey:kCATransactionDisableActions]; 
@@ -140,7 +142,8 @@
 			break;
 		case EGOOPullLoading:
 			
-			_statusLabel.text = NSLocalizedStringFromTable(@"Loading...", @"PullTableViewLan",@"Loading Status");
+//			_statusLabel.text = NSLocalizedStringFromTable(@"Loading...", @"PullTableViewLan",@"Loading Status");
+            _statusLabel.text = NSLocalizedStringFromTable(@"加载中...", @"PullTableViewLan",@"Loading Status");
 			[_activityView startAnimating];
 			[CATransaction begin];
 			[CATransaction setValue:(id)kCFBooleanTrue forKey:kCATransactionDisableActions]; 
